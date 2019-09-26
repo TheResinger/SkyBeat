28c15e9fe1e6c41491eb4e716132d86bde9bf6cb
Benjamin Morrow <resinger@localhost.localdomain>
2019-09-25 22:40:06 -0400

    More user registration and login work

--- public/js/register.js
+++ public/js/register.js
@@ -1,21 +1,32 @@
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-const plaintextPass = $("#password");
-
-const handleFormSubmit = event => {
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    bcrypt.hash(plaintextPass, salt, (err, hash) => {
-      if (err) {
-        throw err;
-      }
-      newUser = hash => {
-        return $.ajax({
-          type: "POST",
-          url: "api/newUser",
-          data: JSON.stringify(hash)
-        });
-      };
-      newUser(hash);
+$(document).ready(() => {
+  let API = {
+    saveUser: user => {
+      return $.ajax({
+        type: "POST",
+        url: "api/newUser",
+        data: JSON.stringify(user)
+      });
+    }
+  };
+  $("#submit").on("click", e => {
+    e.preventDefault();
+    var user = {
+      userName: $("#username")
+        .val()
+        .trim(),
+      email: $("#email")
+        .val()
+        .trim(),
+      password: $("#password")
+        .val()
+        .trim()
+    };
+    console.log(user);
+    API.saveUser(user).then(() => {
+      console.log("USER SAVED");
     });
   });
-}
+});
+// const plaintextPass = $("#password")
+//   .val()
+//   .trim();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
 
 module.exports = function(app) {
   // Get all examples
@@ -9,15 +11,24 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // app.post("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
+  app.post("/api/newUser", (req, res) => {
+    console.log(req.body.userName);
+    // db.User.create(req.body).then(dbUsers => {
+    //   res.json(dbUsers);
+    // });
+    // bcrypt.genSalt(saltRounds, (err,salt) => {
+    //   bcrypt.hash()
+    // })
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(dbExample => {
       res.json(dbExample);
     });
   });
